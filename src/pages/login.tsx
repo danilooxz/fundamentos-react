2b519206d04ac73abe433fc028e18d082647ebe7
Danilo Marcon Júnior <juniordanilo538@gmail.com>
2025-08-05 22:29:00 -0300

    feat: controle de estado do form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,54 +1,75 @@
-import { Button, Center, Flex, Heading, HStack, Image, Input, Stack, Text, VStack, Link as ChakraLink, Field } from "@chakra-ui/react";
+import { Button, Flex, Heading, HStack, Image, Input, Stack, Text, VStack, Link as ChakraLink, Field } from "@chakra-ui/react";
 import loginImage from "../../public/assets/login-image.gif";
 import { Checkbox } from "@/components/ui/checkbox";
 import NextLink from "next/link"
 import { PasswordInput } from "@/components/ui/password-input";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const signInFormSchema = z.object({
+  email: z.email("Digite um e-mail válido").nonempty("O e-mail é obrigatório"),
+  password: z.string().nonempty("A senha é obrigatória").min(8, "A senha deve pelo menos 8 caracteres"),
+});
+
+type signInFormData = z.infer<typeof signInFormSchema>
 
 export default function Login() {
-    return(
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    resolver: zodResolver(signInFormSchema)
+  });
+
+  function handleSignIn(data: signInFormData){
+    console.log(data);
+  }
+
+  return(
     <Flex w="100vw" h="100vh">
-        <Flex w="50%" bg="#2C73EB" align="center" justify="center">
-          <Image w={500} h={500} src={loginImage.src}/>
-        </Flex>
-        <VStack w="50%" justify="center">
-          <Stack>
-            <Heading as="h1" color="black" fontSize="3xl" fontWeight="bold">Login</Heading>
+      <Flex w="50%" bg="#2C73EB" align="center" justify="center">
+        <Image w={500} h={500} src={loginImage.src}/>
+      </Flex>
+      <VStack w="50%" justify="center">
+        <Stack>
+          <Heading as="h1" color="black" fontSize="3xl" fontWeight="bold">Login</Heading>
 
-            <Text color="gray.400" fontSize="lg" fontWeight="normal">Se você já é membro, você pode fazer login com seu endereço de e-mail e senha.</Text>
+          <Text color="gray.400" fontSize="lg" fontWeight="normal">Se você já é membro, você pode fazer login com seu endereço de e-mail e senha.</Text>
 
-            <VStack align="flex-start" gap={6} mt={10}>
-              <Field.Root>
-                <Field.Label color="gray.500" fontSize="md">
-                  Email
-                </Field.Label>
-                <Input type="email" h={16} colorPalette="blue" borderRadius="md" color="black" />
-              </Field.Root>
+          <VStack as="form" onSubmit={handleSubmit((data: signInFormData) => console.log(data))} align="flex-start" gap={6} mt={10}>
+            <Field.Root invalid={!!errors.email}>
+              <Field.Label color="gray.500" fontSize="md">
+                Email
+              </Field.Label>
+              <Input type="email" h={16} colorPalette="blue" borderRadius="md" color="black" {...register("email")} />
+              <Field.ErrorText>{errors.email?.message}</Field.ErrorText>
+            </Field.Root>
 
-              <Field.Root colorPalette={"blue"}>
-                <Field.Label color="gray.500" fontSize="md">
-                  Senha
-                </Field.Label>
-                <PasswordInput h={16} colorPalette="blue" borderRadius="md" color="black" />
-              </Field.Root>
+            <Field.Root invalid={!!errors.password}>
+              <Field.Label color="gray.500" fontSize="md">
+                Senha
+              </Field.Label>
+              <PasswordInput h={16} colorPalette="blue" borderRadius="md" color="black" {...register("password")} />
+              <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+            </Field.Root>
 
-              <Checkbox color="gray.500" colorPalette="blue" fontSize="md" fontWeight="medium">
-                Lembre-me
-              </Checkbox>
+            <Checkbox color="gray.500" colorPalette="blue" fontSize="md" fontWeight="medium">
+              Lembre-me
+            </Checkbox>
 
-              <Button w="full" h={16} colorPalette="blue" borderRadius="md" fontSize="md" fontWeight="medium">
-                Entrar
-              </Button>
-            </VStack>
+            <Button type="submit" w="full" h={16} colorPalette="blue" borderRadius="md" fontSize="md" fontWeight="medium">
+              Entrar
+            </Button>
+          </VStack>
 
-            <HStack justify="center" gap={1} mt={10}>
-              <Text color="gray.500" fontSize="md" fontWeight="medium">Não possui uma conta?</Text>
-              <ChakraLink color="blue.500" asChild>
-                <NextLink href="/sign-up">Clique aqui!</NextLink>
-              </ChakraLink>
-            </HStack>
+          <HStack justify="center" gap={1} mt={10}>
+            <Text color="gray.500" fontSize="md" fontWeight="medium">Não possui uma conta?</Text>
+            <ChakraLink color="blue.500" asChild>
+              <NextLink href="/sign-up">Clique aqui!</NextLink>
+            </ChakraLink>
+          </HStack>
 
-          </Stack>
-        </VStack>
+        </Stack>
+      </VStack>
     </Flex>
   )
 }

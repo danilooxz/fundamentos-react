766c874feaba332e703dfddebfbf3f1114623c49
Danilo Marcon Júnior <juniordanilo538@gmail.com>
2025-08-26 22:36:11 -0300

    drawer menu

--- src/components/DefaultHeader/Menu.tsx
+++ src/components/DefaultHeader/Menu.tsx
@@ -1,11 +1,37 @@
-import { IconButton } from "@chakra-ui/react";
+import { Button, CloseButton, Drawer, IconButton, Kbd, Portal } from "@chakra-ui/react";
 import { IoMdMenu } from "react-icons/io";
 
-export function Menu () {
+export function Menu() {
   return (
-    <IconButton variant="ghost" rounded="full">
-      <IoMdMenu />
-    </IconButton>
+    <Drawer.Root placement="start" size="md">
+      <Drawer.Trigger asChild>
+        <IconButton variant="ghost" rounded="full">
+          <IoMdMenu />
+        </IconButton>
+      </Drawer.Trigger>
 
-  )
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content>
+            <Drawer.Header>
+              <Drawer.Title>Cesul - Sistema Acâdemico</Drawer.Title>
+            </Drawer.Header>
+            <Drawer.Body>
+              Press the <Kbd>esc</Kbd> key to close the drawer.
+            </Drawer.Body>
+            <Drawer.Footer>
+              <Drawer.ActionTrigger asChild>
+                <Button variant="outline">Cancel</Button>
+              </Drawer.ActionTrigger>
+              <Button>Save</Button>
+            </Drawer.Footer>
+            <Drawer.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Drawer.CloseTrigger>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
+  );
 }

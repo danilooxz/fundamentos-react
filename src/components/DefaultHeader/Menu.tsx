e94634f4332fa3fa81fc9d8d394c4a98f2388b09
Danilo Marcon Júnior <juniordanilo538@gmail.com>
2025-08-26 21:47:11 -0300

    refector(ambos coponentes da pagina princial)

---
+++ src/components/DefaultHeader/Menu.tsx
@@ -0,0 +1,11 @@
+import { IconButton } from "@chakra-ui/react";
+import { IoMdMenu } from "react-icons/io";
+
+export function Menu () {
+  return (
+    <IconButton variant="ghost" rounded="full">
+      <IoMdMenu />
+    </IconButton>
+
+  )
+}
--- src/components/DefaultHeader/index.tsx
+++ src/components/DefaultHeader/index.tsx
@@ -1,44 +1,37 @@
-import { Flex, Group, IconButton, Image, Separator } from "@chakra-ui/react";
-import { IoMdHelpCircle, IoMdMenu, IoMdMoon, IoMdNotifications, IoMdSettings, IoMdSunny } from "react-icons/io";
-
-import { Avatar } from "../ui/avatar";
+import { Flex, Group, Separator } from "@chakra-ui/react";
 import { useColorMode } from "../ui/color-mode";
+import { ColorMode } from "./ColorMode";
+import { Help } from "./Help";
 import { Logo } from "./Logo";
+import { Menu } from "./Menu";
+import { Notifications } from "./Notifications";
+import { Profile } from "./Profile";
+import { Settings } from "./Settings";
 
 export function DefaultHeader() {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
     <Flex w="full" justify="space-between" borderBottomWidth={1} py={2} px={4}>
-        <Flex align="center" gap={4}>
-          <IconButton variant="ghost" rounded="full">
-            <IoMdMenu />
-          </IconButton>
+      <Flex align="center" gap={4}>
+        <Menu />
 
-          <Logo/>
-        </Flex>
-
-        <Group>
-          <IconButton variant="ghost" rounded="full">
-            <IoMdNotifications />
-          </IconButton>
-
-          <IconButton variant="ghost" rounded="full">
-            <IoMdHelpCircle />
-          </IconButton>
-
-          <IconButton variant="ghost" rounded="full">
-            <IoMdSettings />
-          </IconButton>
-
-          <IconButton variant="ghost" rounded="full" onClick={toggleColorMode}>
-            {colorMode === 'dark' ? <IoMdMoon /> : <IoMdSunny />}
-          </IconButton>
-
-          <Separator h={6} orientation="vertical" mx={2} />
-
-          <Avatar name="Danilo Marcon" colorPalette="purple" src="https://avatars.githubusercontent.com/u/128654904?v=4" />
-        </Group>
+        <Logo />
       </Flex>
-  )
+
+      <Group>
+        <Notifications />
+
+        <Help />
+
+        <Settings />
+
+        <ColorMode />
+
+        <Separator h={6} orientation="vertical" mx={2} />
+
+        <Profile />
+      </Group>
+    </Flex>
+  );
 }

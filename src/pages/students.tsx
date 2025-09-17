48a774cea613ba254a30de9426922aaeabb7ba61
Danilo Marcon Júnior <juniordanilo538@gmail.com>
2025-09-16 22:38:26 -0300

    começando a tela de estudantes

--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,13 +1,22 @@
-import { Heading } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
 
 export default function Students() {
   return (
     <DefaultLayout
-      tittle="Acadêmico"
-      descripition="Acadêmico"
+      tittle="Gestão de Estudantes"
+      descripition="Gerencie informações dos estudantes matriculados"
     >
-      <Heading color="red"> Students </Heading>
+      <HStack gap={5}>
+        {/*
+          Input - Campo de pesquisa - SearchInput
+
+          Select - Filtro de Status
+          Button - Filtros
+          Button - Exportar
+          Button - Action Button - Criar um novo estudante
+        */}
+      </HStack>
     </DefaultLayout>
   );
 }
